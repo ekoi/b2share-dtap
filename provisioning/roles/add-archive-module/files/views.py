dd3e65c0c1e58a040fd75c663ae8cc37d75d5aa1
ekoi <eko.indarto@dans.knaw.nl>
2019-06-20 13:11:05 +0200

    intermediate commit: working version - GET

--- provisioning/roles/add-archive-module/files/views.py
+++ provisioning/roles/add-archive-module/files/views.py
@@ -19,22 +19,21 @@
 
 from __future__ import absolute_import
 
-from flask import Blueprint, jsonify, current_app, request
+from flask import Blueprint, jsonify, current_app, request, abort
 
 from invenio_rest import ContentNegotiatedMethodView
+from invenio_files_rest import models as fm
+from invenio_records.api import Record
+from b2share.modules.records.providers import RecordUUIDProvider
 
 from b2share import __version__
 
-
 blueprint = Blueprint('b2share_apiarchive', __name__, url_prefix='/archive')
-
 
 class ApiArchive(ContentNegotiatedMethodView):
 
     def __init__(self, resolver=None, **kwargs):
-        print(kwargs)
         """Constructor.
-
         :param resolver: Persistent identifier resolver instance.
         """
         default_media_type = 'application/json'
@@ -43,56 +42,25 @@ class ApiArchive(ContentNegotiatedMethodView):
                 'application/json': lambda response: jsonify(response)
             },
             default_method_media_type={
-                'POST': default_media_type,
+                'GET': default_media_type,
             },
             default_media_type=default_media_type,
             **kwargs)
         self.resolver = resolver
 
     def get(self, **kwargs):
-        b2access = current_app.config.get('OAUTHCLIENT_REMOTE_APPS', {}).get(
-            'b2access', {})
-        data = {
-            'version': __version__,
-            'eko': 'indarto999---',
-            'site_function': current_app.config.get('SITE_FUNCTION', ''),
-            'training_site_link': current_app.config.get('TRAINING_SITE_LINK', ''),
-            'b2access_registration_link': b2access.get('registration_url'),
-            'b2note_url': current_app.config.get('B2NOTE_URL'),
-            'terms_of_use_link': current_app.config.get('TERMS_OF_USE_LINK'),
-        }
-        response = jsonify(data)
-        return response
-
-    def post(self, **kwargs):
-        print(request.data)
-        import json
+        r = request.args.get('r')
         try:
-            import urllib2 as http
-        except ImportError:
-            from urllib import request as http
-
-        print('--0--')
-        try:
-            print('--1--')
-            response = http.urlopen('http://localhost:5000/vag')
-            content = response.read()
-            response.close()
-            print('--2--')
-            dict_content = json.loads(content.decode('utf-8'))
-            print(content)
-
-        except http.HTTPError as response:
-            print('--error--')
-            content = response.read()
-            response.close()
-
-        data = {
-            "dans": 'knaw',
-        }
-        response = jsonify(data)
-        return self.make_response({
-            'message':'The record is -----reported.'
-        })
+            rec_pid = RecordUUIDProvider.get(r).pid
+            record = Record.get_record(rec_pid.object_uuid)
+            for f in record.get('_files', []):
+                fmimetype = fm.ObjectVersion.get(f.get('bucket'), f.get('key'), f.get('version_id')).mimetype
+                pid = f.get('bucket')
+                f.update({'mimetype': fmimetype})
+                fileLocation = current_app.config.get('PREFERRED_URL_SCHEME', '') + '://' + current_app.config.get('JSONSCHEMAS_HOST', '') + '/api/files/' + f.get('bucket') + '/' + f.get('key')
+                f.update({'file-location': fileLocation})
+            return record
+        except:
+            return abort(404)
 
 blueprint.add_url_rule('/', view_func=ApiArchive.as_view('info'))
